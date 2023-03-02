c735ebbe07bb02b741a3f8af2c9d0cf6a6a451fe
Slipninja <26084911+SlipNinja@users.noreply.github.com>
2023-03-02 21:50:37 +0100

    Add delete button on books

--- scripts.js
+++ scripts.js
@@ -11,11 +11,69 @@ function Book(title, author, pages, read) {
     }
 }
 
+Book.prototype.changeReadStatus = function () {
+    this.read = !this.read;
+}
+
 const library = [];
 
 function addBook(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     library.push(book);
+
+    return book;
+}
+
+function createCard(book) {
+    let bookDisplay = document.createElement("div");
+    bookDisplay.classList.add("book");
+    bookDisplay.dataset.index = library.indexOf(book);
+
+    let title = document.createElement("div");
+    let titleContent = document.createTextNode(book.title);
+    title.style["font-weight"] = "bold";
+    title.appendChild(titleContent);
+
+    let author = document.createElement("div");
+    let authorContent = document.createTextNode(`by ${book.author}`);
+    author.appendChild(authorContent);
+
+    let pages = document.createElement("div");
+    let pagesContent = document.createTextNode(`${book.pages} pages`);
+    pages.appendChild(pagesContent);
+
+    let read = document.createElement("div");
+    let readDisplay = book.read ? "Already read" : "Not read yet";
+    let readContent = document.createTextNode(readDisplay);
+    read.appendChild(readContent);
+
+    let delBtn = document.createElement("button");
+    let delBtnText = document.createTextNode("Delete");
+    delBtn.appendChild(delBtnText);
+    delBtn.onclick = deleteBook;
+
+    bookDisplay.appendChild(title);
+    bookDisplay.appendChild(author);
+    bookDisplay.appendChild(pages);
+    bookDisplay.appendChild(read);
+    bookDisplay.appendChild(delBtn);
+
+    return bookDisplay;
+}
+
+function deleteBook(e) {
+    let bookToDelete = e.srcElement.parentElement;
+    let index = bookToDelete.dataset.index;
+
+    library.splice(index, 1);
+    bookToDelete.remove();
+
+    let books = document.getElementsByClassName("book");
+    let i = 0;
+    for (let bookDisplay of books){
+        bookDisplay.dataset.index = i++;
+    }
+    //console.log(library);
 }
 
 addBook("The beginning", "Mickael Jackson", 3, false);
@@ -23,37 +81,32 @@ addBook("The middle earth", "Tolkien", 300, true);
 addBook("How to raise a snake", "Macaron", 666, false);
 
 library.forEach(function (book) {
-    //console.log(book.info());
-    let bookDisplay = document.createElement("div");
-    bookDisplay.classList.add("book");
+    let bookDisplay = createCard(book);
 
-    let title = document.createElement("div");
-    let titleContent = document.createTextNode(book.title);
-    title.appendChild(titleContent);
-
-    let author = document.createElement("div");
-    let authorContent = document.createTextNode(book.author);
-    author.appendChild(authorContent);
-
-    let pages = document.createElement("div");
-    let pagesContent = document.createTextNode(book.pages);
-    pages.appendChild(pagesContent);
-
-    let read = document.createElement("div");
-    let readContent = document.createTextNode(book.read);
-    read.appendChild(readContent);
-
-    bookDisplay.appendChild(title);
-    bookDisplay.appendChild(author);
-    bookDisplay.appendChild(pages);
-    bookDisplay.appendChild(read);
-
-    let lib = document.getElementsByClassName("library")[0];
+    let lib = document.getElementById("library");
     lib.appendChild(bookDisplay);
 });
 
-function showForm() {
-    const form = document.getElementById("addForm");
+const addForm = document.getElementById("addForm");
 
-    form.style.display = (!form.style.display || form.style.display == "none") ? "flex" : "none";
+addForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = +document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+
+    let newBook = addBook(title, author, pages, read);
+    let bookDisplay = createCard(newBook);
+
+    let lib = document.getElementById("library");
+    lib.appendChild(bookDisplay);
+
+    event.target.reset();
+    event.target.style.display = "none";
+});
+
+function showForm() {
+    addForm.style.display = (!addForm.style.display || addForm.style.display == "none") ? "flex" : "none";
 }
